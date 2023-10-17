8f2df2ce63d7884ecf0eafeb6851d06edcbdf709
pasindu-gamarachchi <pgamarachchi@gmail.com>
2023-10-16 23:46:37 -0400

    data downloader ok

---
+++ etl/Etl_logger/etl_logger.py
@@ -0,0 +1,17 @@
+import logging
+from datetime import  datetime
+import time
+
+etl_Logger = logging.getLogger(__name__)
+steamHandler = logging.StreamHandler()
+timestamp = datetime.now().strftime("%a_%d%-m%-Y_%H%M")
+fileHandler = logging.FileHandler(f"logs/{timestamp}.log")
+streamFormat = logging.Formatter('%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s')
+
+steamHandler.setFormatter(streamFormat)
+fileHandler.setFormatter(streamFormat)
+steamHandler.setLevel(logging.INFO)
+etl_Logger.addHandler(steamHandler)
+etl_Logger.addHandler(fileHandler)
+
+etl_Logger.setLevel(logging.DEBUG)
